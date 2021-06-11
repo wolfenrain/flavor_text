9ea12069a899b07cff4488382e9fb6dc02a54195
Jochum van der Ploeg <jochum@vdploeg.net>
2021-06-11 08:36:14 +0200

    Added support for all know icons

--- lib/src/tags/icon_tag.dart
+++ lib/src/tags/icon_tag.dart
@@ -1,33 +1,29 @@
 part of flavor_text;
 
-/// Tag for adding icons as text (not as widget).
-/// 
-/// By parsing the [IconData.codePoint] as a char code and setting the 
-/// fontFamily to the [IconData.fontFamily] we can render icons as text.
-/// 
+/// Tag for displaying icons as text.
+///
+/// By using the icon name as the text and setting the fontFamily
+/// to MaterialIcons wen we can render icons as text.
+///
+/// See [Icons] for a list of icons that are supported.
+///
 /// ```dart
 /// final text1 = '<icon>home</icon>';
 /// final text2 = '<icon>phone</icon>';
 /// ```
 class IconTag extends Tag {
   @override
-  List<String> get supportedProperties => [];
+  List<String> get supportedProperties => ['family'];
 
   @override
   InlineSpan build(BuildContext context) {
-    final icon = when(text, {
-      'threesixty': () => Icons.threesixty,
-      'home': () => Icons.home,
-      'phone': () => Icons.phone,
-    });
-
-    if (icon == null) {
-      return TextSpan();
-    }
-
     return TextSpan(
-      text: String.fromCharCode(icon.codePoint),
-      style: TextStyle(fontFamily: icon.fontFamily),
+      text: text,
+      style: TextStyle(
+        fontFamily: when(properties['family'], {
+          'CupertinoIcons': () => 'CupertinoIcons',
+        }).orElse(() => 'MaterialIcons'),
+      ),
     );
   }
 }
